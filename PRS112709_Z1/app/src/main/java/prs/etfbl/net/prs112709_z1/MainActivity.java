8dac0051a64ab1be74db16ce3424203237677e83
Milna <mbojic@hotmail.com>
2016-04-10 22:38:30 +0200

    Novi projekat

---
+++ PRS112709_Z1/app/src/main/java/prs/etfbl/net/prs112709_z1/MainActivity.java
@@ -0,0 +1,13 @@
+package prs.etfbl.net.prs112709_z1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
